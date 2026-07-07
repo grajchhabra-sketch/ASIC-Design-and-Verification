7ada61fa39a70d037b05a0f1e3f061f412373f26
Gautam Raj Chhabra <grajchhabra@gmail.com>
2026-07-07 13:32:40 +0530

    Add 11_test.sv to C_UVM_Testbench

---
+++ AXI-MultiMaster-Memory-Subsystem/C_UVM_Testbench/11_test.sv
@@ -0,0 +1,29 @@
+   class axi_test extends uvm_test;
+
+    `uvm_component_utils(axi_test)
+
+    axi_env env;
+    axi_sequence seq;
+
+    function new(string name = "axi_test", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+
+        env = axi_env::type_id::create("env", this);
+        seq = axi_sequence::type_id::create("seq");
+    endfunction
+
+    task run_phase(uvm_phase phase);
+
+        phase.raise_objection(this);
+
+        seq.start(env.agent.sequencer);
+
+        phase.drop_objection(this);
+
+    endtask
+
+endclass
