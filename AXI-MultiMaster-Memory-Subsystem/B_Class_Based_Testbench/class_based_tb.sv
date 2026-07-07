686114e3babdcd635f8438b74fd479fa8e207e20
Gautam Raj Chhabra <grajchhabra@gmail.com>
2026-07-07 13:48:32 +0530

    Refactor AXI testbench to use soc_tx class

--- AXI-MultiMaster-Memory-Subsystem/B_Class_Based_Testbench/class_based_tb.sv
+++ AXI-MultiMaster-Memory-Subsystem/B_Class_Based_Testbench/class_based_tb.sv
@@ -1,23 +1,28 @@
-interface axi_if #(parameter ADDR_WIDTH = 32,
+interface soc_if #(parameter ADDR_WIDTH = 32,
                    parameter DATA_WIDTH = 32);
 
     logic clk;
     logic reset;
 
+
     logic [ADDR_WIDTH-1:0] awaddr;
     logic                  awvalid;
     logic                  awready;
+
 
     logic [DATA_WIDTH-1:0] wdata;
     logic                  wvalid;
     logic                  wready;
 
+
     logic                  bvalid;
     logic                  bready;
+
 
     logic [ADDR_WIDTH-1:0] araddr;
     logic                  arvalid;
     logic                  arready;
+
 
     logic [DATA_WIDTH-1:0] rdata;
     logic                  rvalid;
@@ -25,8 +30,7 @@ interface axi_if #(parameter ADDR_WIDTH = 32,
 
 endinterface
 
-
-class axi_transaction extends uvm_sequence_item;
+class soc_tx;
 
     rand bit awvalid;
     rand bit [31:0] awaddr;
@@ -43,11 +47,7 @@ class axi_transaction extends uvm_sequence_item;
     rand bit [1:0] aw_region;
     rand bit [1:0] ar_region;
 
-    `uvm_object_utils(axi_transaction)
 
-    function new(string name = "axi_transaction");
-        super.new(name);
-    endfunction
 
     constraint c_rw
     {
@@ -73,6 +73,7 @@ class axi_transaction extends uvm_sequence_item;
         rready dist {1:=80,0:=20};
     }
 
+  
     constraint c_region
     {
         aw_region inside {0,1,2};
@@ -82,12 +83,19 @@ class axi_transaction extends uvm_sequence_item;
     constraint c_data
     {
         wdata dist {
+
             [32'h00000000:32'h3FFFFFFF] := 25,
+
             [32'h40000000:32'h7FFFFFFF] := 25,
+
             [32'h80000000:32'hBFFFFFFF] := 25,
+
             [32'hC0000000:32'hFFFFFFFF] := 25
+
         };
     }
+
+   
 
     function void post_randomize();
 
@@ -130,51 +138,96 @@ class axi_transaction extends uvm_sequence_item;
 
     endfunction
 
+
     function void display();
 
         if(awvalid)
             $display("WRITE ADDR=%h DATA=%h",awaddr,wdata);
+
         else
-            $display("READ ADDR=%h",araddr);
+            $display("READ  ADDR=%h",araddr);
 
     endfunction
 
 endclass
+class driver;
 
+  virtual soc_if vif;
+  mailbox #(soc_tx) gen2drv;
 
-class axi_sequence extends uvm_sequence #(axi_transaction);
+  function new(virtual soc_if vif,
+               mailbox #(soc_tx) gen2drv);
+    this.vif = vif;
+    this.gen2drv = gen2drv;
+  endfunction
 
-    `uvm_object_utils(axi_sequence)
+  task run();
 
-    int num_tx = 200;
+    soc_tx tx;
 
-    function new(string name = "axi_sequence");
-        super.new(name);
+    wait(vif.reset == 0);
+
+    forever begin
+
+      gen2drv.get(tx);
+
+      @(posedge vif.clk);
+
+      vif.awaddr  <= tx.awaddr;
+      vif.awvalid <= tx.awvalid;
+
+      vif.wdata   <= tx.wdata;
+      vif.wvalid  <= tx.wvalid;
+
+      vif.araddr  <= tx.araddr;
+      vif.arvalid <= tx.arvalid;
+
+      vif.bready  <= tx.bready;
+      vif.rready  <= tx.rready;
+
+      @(posedge vif.clk);
+
+      vif.awvalid <= 0;
+      vif.wvalid  <= 0;
+      vif.arvalid <= 0;
+
+    end
+
+  endtask
+
+endclass : driver
+
+class generator;
+
+    mailbox #(soc_tx) gen2drv;
+
+    int num_tx = 30000;
+
+    function new(mailbox #(soc_tx) gen2drv);
+        this.gen2drv = gen2drv;
     endfunction
 
-    task body();
+    task run();
 
-        axi_transaction tx;
-
+        soc_tx tx;
         repeat(num_tx)
         begin
-            tx = axi_transaction::type_id::create("tx");
-
-            start_item(tx);
+            tx = new();
 
             assert(tx.randomize())
             else
-                `uvm_fatal("SEQ", "Randomization Failed")
+                $fatal("Randomization Failed");
 
-            finish_item(tx);
+            gen2drv.put(tx);
         end
 
-        directed_write(32'h00000000, 32'h00000000, 1);
-        directed_write(32'h00000004, 32'h50000000, 0);
-        directed_write(32'h00008000, 32'h90000000, 1);
-        directed_write(32'h00008004, 32'hF0000000, 0);
-        directed_write(32'hFFFFFFFC, 32'h00000001, 1);
-        directed_write(32'hFFFFFFF8, 32'hFFFFFFFF, 0);
+        directed_write(32'h00000000, 32'h00000000, 1); 
+        directed_write(32'h00000004, 32'h50000000, 0); 
+        directed_write(32'h00008000, 32'h90000000, 1); 
+        directed_write(32'h00008004, 32'hF0000000, 0); 
+        directed_write(32'hFFFFFFFC, 32'h00000001, 1); 
+        directed_write(32'hFFFFFFF8, 32'hFFFFFFFF, 0); 
+
 
         directed_read(32'h00000000, 1);
         directed_read(32'h00000004, 0);
@@ -185,16 +238,10 @@ class axi_sequence extends uvm_sequence #(axi_transaction);
 
     endtask
 
-    task directed_write(bit [31:0] addr,
-                        bit [31:0] data,
-                        bit bready_val);
-
-        axi_transaction tx;
-
-        tx = axi_transaction::type_id::create("tx");
-
-        start_item(tx);
-
+ 
+    task directed_write(bit [31:0] addr, bit [31:0] data, bit bready_val);
+        soc_tx tx;
+        tx = new();
         tx.awvalid = 1;
         tx.arvalid = 0;
         tx.wvalid  = 1;
@@ -202,239 +249,135 @@ class axi_sequence extends uvm_sequence #(axi_transaction);
         tx.wdata   = data;
         tx.bready  = bready_val;
         tx.rready  = 1;
-
-        finish_item(tx);
-
+        gen2drv.put(tx);
     endtask
 
-    task directed_read(bit [31:0] addr,
-                       bit rready_val);
-
-        axi_transaction tx;
-
-        tx = axi_transaction::type_id::create("tx");
-
-        start_item(tx);
-
+    task directed_read(bit [31:0] addr, bit rready_val);
+        soc_tx tx;
+        tx = new();
         tx.awvalid = 0;
         tx.arvalid = 1;
         tx.araddr  = addr;
         tx.bready  = 1;
         tx.rready  = rready_val;
-
-        finish_item(tx);
-
+        gen2drv.put(tx);
     endtask
 
 endclass
+class monitor;
 
-class axi_sequencer extends uvm_sequencer #(axi_transaction);
+  virtual soc_if vif;
 
-    `uvm_component_utils(axi_sequencer)
+  mailbox #(soc_tx) mon2scb;
+  mailbox #(soc_tx) mon2cov;
 
-    function new(string name = "axi_sequencer", uvm_component parent);
-        super.new(name, parent);
-    endfunction
+  function new(
+      virtual soc_if vif,
+      mailbox #(soc_tx) mon2scb,
+      mailbox #(soc_tx) mon2cov
+  );
+      this.vif     = vif;
+      this.mon2scb = mon2scb;
+      this.mon2cov = mon2cov;
+  endfunction
 
-endclass
 
+  task run();
 
-class axi_driver extends uvm_driver #(axi_transaction);
+      soc_tx tx;
 
-    `uvm_component_utils(axi_driver)
+      wait(vif.reset == 0);
 
-    virtual axi_if vif;
+      forever begin
 
-    function new(string name = "axi_driver", uvm_component parent);
-        super.new(name, parent);
-    endfunction
+          @(posedge vif.clk);
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
+          if(vif.awvalid || vif.arvalid) begin
 
-        if(!uvm_config_db#(virtual axi_if)::get(this, "", "vif", vif))
-            `uvm_fatal("DRV", "Virtual Interface Not Found")
-    endfunction
+              tx = new();
 
-    task run_phase(uvm_phase phase);
+           
+              tx.awvalid = vif.awvalid;
+              tx.awaddr  = vif.awaddr;
 
-        axi_transaction tx;
+              tx.wvalid  = vif.wvalid;
+              tx.wdata   = vif.wdata;
 
-        wait(vif.reset == 0);
+          
+              tx.arvalid = vif.arvalid;
+              tx.araddr  = vif.araddr;
 
-        forever begin
+           
+              tx.bready = vif.bready;
+              tx.rready = vif.rready;
 
-            seq_item_port.get_next_item(tx);
+             
+              mon2scb.put(tx);
+              mon2cov.put(tx);
 
-            @(posedge vif.clk);
+          end
 
-            vif.awaddr  <= tx.awaddr;
-            vif.awvalid <= tx.awvalid;
+      end
 
-            vif.wdata   <= tx.wdata;
-            vif.wvalid  <= tx.wvalid;
-
-            vif.araddr  <= tx.araddr;
-            vif.arvalid <= tx.arvalid;
-
-            vif.bready  <= tx.bready;
-            vif.rready  <= tx.rready;
-
-            @(posedge vif.clk);
-
-            vif.awvalid <= 0;
-            vif.wvalid  <= 0;
-            vif.arvalid <= 0;
-
-            seq_item_port.item_done();
-
-        end
-
-    endtask
+  endtask
 
 endclass
-      
-      
-      class axi_monitor extends uvm_monitor;
+class scoreboard;
 
-    `uvm_component_utils(axi_monitor)
+  mailbox #(soc_tx) mon2scb;
 
-    virtual axi_if vif;
+  function new(mailbox #(soc_tx) mon2scb);
+    this.mon2scb = mon2scb;
+    
+  endfunction
 
-    uvm_analysis_port #(axi_transaction) mon2scb;
-    uvm_analysis_port #(axi_transaction) mon2cov;
+  task run();
 
-    function new(string name = "axi_monitor", uvm_component parent);
-        super.new(name, parent);
+    soc_tx tx;
 
-        mon2scb = new("mon2scb", this);
-        mon2cov = new("mon2cov", this);
-    endfunction
+    forever begin
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
+      mon2scb.get(tx);
 
-        if(!uvm_config_db#(virtual axi_if)::get(this, "", "vif", vif))
-            `uvm_fatal("MON", "Virtual Interface Not Found")
-    endfunction
+     
+      $display("SCOREBOARD");
 
-    task run_phase(uvm_phase phase);
+      // Write Transaction
+      if(tx.awvalid)
+      begin
+        $display("WRITE TRANSACTION");
+        $display("Address : %h", tx.awaddr);
+        $display("Data    : %h", tx.wdata);
 
-        axi_transaction tx;
-
-        wait(vif.reset == 0);
-
-        forever begin
-
-            @(posedge vif.clk);
-
-            if(vif.awvalid || vif.arvalid) begin
-
-                tx = axi_transaction::type_id::create("tx");
-
-                tx.awvalid = vif.awvalid;
-                tx.awaddr  = vif.awaddr;
-
-                tx.wvalid  = vif.wvalid;
-                tx.wdata   = vif.wdata;
-
-                tx.arvalid = vif.arvalid;
-                tx.araddr  = vif.araddr;
-
-                tx.bready  = vif.bready;
-                tx.rready  = vif.rready;
-
-                mon2scb.write(tx);
-                mon2cov.write(tx);
-
-            end
-
-        end
-
-    endtask
-
-endclass
-      
-      
-      class axi_agent extends uvm_agent;
-
-    `uvm_component_utils(axi_agent)
-
-    axi_sequencer sequencer;
-    axi_driver    driver;
-    axi_monitor   monitor;
-
-    function new(string name = "axi_agent", uvm_component parent);
-        super.new(name, parent);
-    endfunction
-
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-
-        sequencer = axi_sequencer::type_id::create("sequencer", this);
-        driver    = axi_driver::type_id::create("driver", this);
-        monitor   = axi_monitor::type_id::create("monitor", this);
-    endfunction
-
-    function void connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-
-        driver.seq_item_port.connect(sequencer.seq_item_export);
-    endfunction
-
-endclass
-      
-      
-      
-      class axi_scoreboard extends uvm_scoreboard;
-
-    `uvm_component_utils(axi_scoreboard)
-
-    uvm_analysis_imp #(axi_transaction, axi_scoreboard) mon2scb;
-
-    function new(string name = "axi_scoreboard", uvm_component parent);
-        super.new(name, parent);
-        mon2scb = new("mon2scb", this);
-    endfunction
-
-    function void write(axi_transaction tx);
-
-        $display("SCOREBOARD");
-
-        if(tx.awvalid)
-        begin
-            $display("WRITE TRANSACTION");
-            $display("Address : %h", tx.awaddr);
-            $display("Data    : %h", tx.wdata);
-
-            if(tx.wvalid)
-                $display("RESULT  : WRITE PASS");
-            else
-                $display("RESULT  : WRITE FAIL (WDATA Missing)");
-        end
-
-        else if(tx.arvalid)
-        begin
-            $display("READ TRANSACTION");
-            $display("Address : %h", tx.araddr);
-            $display("RESULT  : READ PASS");
-        end
-
+        if(tx.wvalid)
+          $display("RESULT  : WRITE PASS");
         else
-        begin
-            $display("NO VALID TRANSACTION");
-        end
+          $display("RESULT  : WRITE FAIL (WDATA Missing)");
+      end
 
-    endfunction
+      // Read Transaction
+      else if(tx.arvalid)
+      begin
+        $display("READ TRANSACTION");
+        $display("Address : %h", tx.araddr);
+        $display("RESULT  : READ PASS");
+      end
+
+      else
+      begin
+        $display("NO VALID TRANSACTION");
+      end
+
+    end
+
+  endtask
 
 endclass
       
-      
-      class axi_coverage extends uvm_subscriber #(axi_transaction);
+class coverage;
 
-    `uvm_component_utils(axi_coverage)
-
-    axi_transaction tx;
+    mailbox #(soc_tx) mon2cov;
+    soc_tx tx;
 
     covergroup axi_cg;
         option.per_instance = 1;
@@ -487,19 +430,19 @@ endclass
 
     endgroup
 
-    function new(string name = "axi_coverage", uvm_component parent);
-        super.new(name, parent);
+    function new(mailbox #(soc_tx) mon2cov);
+        this.mon2cov = mon2cov;
         axi_cg = new();
     endfunction
 
-    function void write(axi_transaction t);
-        tx = t;
-        axi_cg.sample();
-    endfunction
+    task run();
+        forever begin
+            mon2cov.get(tx);
+            axi_cg.sample();
+        end
+    endtask
 
-    function void report_phase(uvm_phase phase);
-        super.report_phase(phase);
-
+    function void report();
         $display("FUNCTIONAL COVERAGE = %0.2f %%", axi_cg.get_coverage());
         $display("cp_write   = %0.2f", axi_cg.cp_write.get_coverage());
         $display("cp_read    = %0.2f", axi_cg.cp_read.get_coverage());
@@ -508,104 +451,128 @@ endclass
         $display("cp_wdata   = %0.2f", axi_cg.cp_wdata.get_coverage());
         $display("cp_bready  = %0.2f", axi_cg.cp_bready.get_coverage());
         $display("cp_rready  = %0.2f", axi_cg.cp_rready.get_coverage());
-       
     endfunction
 
 endclass
-      
-      
-      class axi_env extends uvm_env;
+class environment;
 
-    `uvm_component_utils(axi_env)
+  generator   gen;
+  driver      drv;
+  monitor     mon;
+  scoreboard  scb;
+  coverage    cov;
 
-    axi_agent      agent;
-    axi_scoreboard scb;
-    axi_coverage   cov;
+  mailbox #(soc_tx) gen2drv;
+  mailbox #(soc_tx) mon2scb;
+  mailbox #(soc_tx) mon2cov;
 
-    function new(string name = "axi_env", uvm_component parent);
-        super.new(name, parent);
-    endfunction
+  virtual soc_if vif;
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
+  function new(virtual soc_if vif);
 
-        agent = axi_agent::type_id::create("agent", this);
-        scb   = axi_scoreboard::type_id::create("scb", this);
-        cov   = axi_coverage::type_id::create("cov", this);
-    endfunction
+    this.vif = vif;
 
-    function void connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
+    gen2drv = new();
+    mon2scb = new();
+    mon2cov = new();
 
-        agent.monitor.mon2scb.connect(scb.mon2scb);
-        agent.monitor.mon2cov.connect(cov.analysis_export);
-    endfunction
+    gen = new(gen2drv);
+    drv = new(vif, gen2drv);
+    mon = new(vif, mon2scb, mon2cov);
+    scb = new(mon2scb);
+    cov = new(mon2cov);
 
-endclass
-      
-      
-      class axi_test extends uvm_test;
+  endfunction
 
-    `uvm_component_utils(axi_test)
-
-    axi_env env;
-    axi_sequence seq;
-
-    function new(string name = "axi_test", uvm_component parent);
-        super.new(name, parent);
-    endfunction
-
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-
-        env = axi_env::type_id::create("env", this);
-        seq = axi_sequence::type_id::create("seq");
-    endfunction
-
-    task run_phase(uvm_phase phase);
-
-        phase.raise_objection(this);
-
-        seq.start(env.agent.sequencer);
-
-        phase.drop_objection(this);
-
-    endtask
+  task run();
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      scb.run();
+      cov.run();
+    join_none
+  endtask
 
 endclass
-      
-      
-      module tb_top;
+class test;
 
-    import uvm_pkg::*;
-    `include "uvm_macros.svh"
+  environment env;
 
-    axi_if vif();
+  virtual soc_if vif;
 
-    top_axi_system dut (
-        .clk(vif.clk),
-        .reset(vif.reset)
-    );
+  function new(virtual soc_if vif);
 
-    initial begin
-        vif.clk = 0;
-        forever #5 vif.clk = ~vif.clk;
-    end
+    this.vif = vif;
 
-    initial begin
-        vif.reset = 1;
-        #20;
-        vif.reset = 0;
-    end
+    env = new(vif);
 
-    initial begin
-        uvm_config_db#(virtual axi_if)::set(null, "*", "vif", vif);
-        run_test("axi_test");
-    end
+  endfunction
 
-    initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars(0, tb_top);
-    end
+  task run();
+
+    $display(" SOC SYSTEM TEST STARTED");
+
+    env.run();
+    
+  endtask
+
+  function void report();
+    env.cov.report();
+  endfunction
+
+endclass
+
+module tb_top;
+
+  soc_if vif();
+
+
+  top_axi_system dut(
+      .clk(vif.clk),
+      .reset(vif.reset)
+  );
+
+ 
+  test t;
+
+
+  initial begin
+      vif.clk = 0;
+      forever #5 vif.clk = ~vif.clk;
+  end
+
+  initial begin
+      vif.reset = 1'b1;
+      #20;
+      vif.reset = 1'b0;
+  end
+
+  
+
+  initial begin
+
+      t = new(vif);
+
+      t.run();
+
+  end
+
+  initial begin
+      #10000;
+      t.report();
+      $display("\nSimulation Finished");
+      $finish;
+  end
+
+ 
+  initial begin
+      $dumpfile("wave.vcd");
+      $dumpvars(0, tb_top);
+  end
 
 endmodule
+      
+      
+      
+      
